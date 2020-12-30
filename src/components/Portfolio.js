71f835b8ab91fb97bc71201e89c88e23d13ea26f
jesseziros <jtziros2020@gmail.com>
2020-12-30 16:46:07 -0500

    Added Github Repository links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,6 +9,7 @@ function Portfolio() {
       name: "Find your Brewery",
       image: Project1,
       link: "https://jameslee70.github.io/FirstProject/",
+      Github: "https://github.com/jameslee70/FirstProject",
       message: "My team decided to use an API called openbrewerydb to locate different bars within an area, as well as utilizing the openweathermap API for the veiwer to see the current weather in the searched area. We then input a few search bars to narrow the search for said breweries/bars. Once all of the information from the openbrewerydb API shows up on the screen we also provided a link to the breweries website. Click the Picture to view the App!",
       techologies: "Javascript, JQuery, Ajax Call, Third Party API's"
     },
@@ -16,6 +17,7 @@ function Portfolio() {
       name: "Doctors Connect",
       image: Project2,
       link: "https://creepy-treat-09091.herokuapp.com/",
+      Github: "https://github.com/tedwar52/Project-2",
       message: "As a team we decided to design an application that could assist in connecting doctors to other for second opinions. Once loaded you will be taken to a landing page with some information about the website. There you will see links the a Search function, Scheduled Appointments and a Logout function at the top, and at the bottom you will see a sign up bar. Once you sign in/create an account you will be taken to the search page where you can search a specific practice, doctor etc. Once you have a set appointment with them you can then keep track of it by inputting the information in the Scheduled Appointment page. Click the Picture to view the App!",
       techologies: "Node.js, MySQL/Sequelize, SASS, Bcrypt"
     },
@@ -23,6 +25,7 @@ function Portfolio() {
       name: "Fitness Tracker",
       image: Project3,
       link: "https://gentle-crag-01569.herokuapp.com/?id=5fd80f193f39be0017214a1a",
+      Github: "https://github.com/jesseziros/Fitness-Tracker",
       message: "In my designing of this app, my duty was to get the routes work and to get the database created. Upon loading the app you will be directed to the fitness tracker page where your last workout will be displayed with all of it's information. From there you can create a new workout or continue your existing one. Creating a new one will give you 2 options of workout, Aerobic or Resistance, from there you will put in all needed information. When you click the 'Complete Workout' button it will save all of your information from that workout and display it on the home page. At the top left if you click on the 'Dashboard' link you will be taken to a page where all of your information is stored from all previous workouts and neatly decorated bar/circle graphs.",
       techologies: "Node.js, Api/Html routes, MongoDB/Mongoose"
     },
@@ -30,7 +33,7 @@ function Portfolio() {
   return (
     <div>
       {projectArray.map(project => {
-        return <Project name={project.name}  key={project.name} link={project.link} image={project.image} message={project.message} techologies={project.techologies} />
+        return <Project name={project.name}  key={project.name} link={project.link} image={project.image} message={project.message} Github={project.Github} techologies={project.techologies} />
       })}
     </div>
   );
--- src/components/Project.js
+++ src/components/Project.js
@@ -8,6 +8,7 @@ function Project(props) {
               <h3>{props.name}</h3>
               <a href={props.link}><img src={props.image} alt=""/></a>
               <h6>{props.message}</h6>
+              <a href={props.Github}>Github Repository: {props.Github}</a>
               <h5>Techologies: {props.techologies}</h5>
             </div>
           </div>
