58b69d354b64fe9a075c4032844d997f8551dbac
jesseziros <jtziros2020@gmail.com>
2020-12-30 15:25:43 -0500

    Added path to Router for deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router path={process.env.PUBLIC_URL + '/'}>
       <div>
         <Navbar />
         <Switch>
